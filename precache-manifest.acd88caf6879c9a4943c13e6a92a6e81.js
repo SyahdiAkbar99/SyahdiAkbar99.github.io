4bf354e18851d1eb41d72a45209e4e45510fea24
SyahdiAkbar99 <58156854+SyahdiAkbar99@users.noreply.github.com>
2020-10-27 21:55:45 -0700

    salin folder

---
+++ precache-manifest.acd88caf6879c9a4943c13e6a92a6e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b171e649f4b5a2415e7",
+    "url": "/static/css/main.75704d04.chunk.css"
+  },
+  {
+    "revision": "6b171e649f4b5a2415e7",
+    "url": "/static/js/main.6b171e64.chunk.js"
+  },
+  {
+    "revision": "7dc6bfac0cd7de58f5b9",
+    "url": "/static/js/1.7dc6bfac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5555ae1e824e93916b22b1068be20229",
+    "url": "/index.html"
+  }
+];
